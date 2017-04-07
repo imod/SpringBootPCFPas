24ce425136be8d4ac034379827c25457b34d20cc
imod <domi@fortysix.ch>
2017-04-07 15:15:01 +0200

    Fix path to file

--- deploy_wrapper.groovy
+++ deploy_wrapper.groovy
@@ -4,7 +4,6 @@ def installApps(deployScript, config) {
                     [$class: 'StringBinding', credentialsId: 'SPRING_CLOUD_CONFIG_ENCRYPT_KEY', variable: 'ENCRYPT_KEY'],
                     [$class: 'UsernamePasswordMultiBinding', credentialsId: 'CLOUDFOUNDRY_USER', usernameVariable: 'CF_USERNAME', passwordVariable: 'CF_PASSWORD']]) {
 
-    unarchive mapping: ['**/*.war,**/*.jar,TODO.md' : '.']
 
     sh """
         cf login -a ${config.cfApiEndpoint} -u $env.CF_USERNAME -p $env.CF_PASSWORD -o ${config.cfOrg} -s ${config.cfSpace}
@@ -21,7 +20,7 @@ def installApps(deployScript, config) {
     parallel (
       "yoo-actuator-test": {
         // API app needs to be up 24/7, therefore do a green blue deployment
-        deployScript.blueGreenDeployment(config.mainCfDomain, env.CONFIG_PASSWORD, env.ENCRYPT_KEY, config.apiAppName, config.springProfile, 'web.manifest.yml', './yooture.web/target/yooture.web.jar', config.apiPublicDomains, config.apiServicesToBind, config.apiInstances)
+        deployScript.blueGreenDeployment(config.mainCfDomain, env.CONFIG_PASSWORD, env.ENCRYPT_KEY, config.apiAppName, config.springProfile, 'manifest.yml', './target/springbootpcfpas-0.0.1-SNAPSHOT.jar', config.apiPublicDomains, config.apiServicesToBind, config.apiInstances)
       },
       failFast: true
     )
